4a7256d5cf6600958f48fa1ee87ed44547c6508f
Artem <Artyom-ARS@users.noreply.github.com>
2020-03-31 14:03:08 +0300

    new working cart pole RL example

--- cartpole/main.py
+++ cartpole/main.py
@@ -1,121 +1,118 @@
-import tensorflow as tf
-import numpy as np
-import random
 import gym
-import math
+import atari_py
+import random
+import numpy as np
+from keras.models     import Sequential
+from keras.layers     import Dense
+from keras.optimizers import Adam
+
+# print(atari_py.list_games())
+# all_envs = gym.envs.registry.all()
+# env_ids = [env_spec.id for env_spec in all_envs]
+# print(env_ids)
+
+# env = gym.make('Breakout-v0')
+# env = gym.make('MsPacman-v0')
+env = gym.make('CartPole-v1')
+
+env.reset()
+goal_steps = 500
+score_requirement = 60
+intial_games = 10000
+
+def model_data_preparation():
+    training_data = []
+    accepted_scores = []
+    for game_index in range(intial_games):
+        score = 0
+        game_memory = []
+        previous_observation = []
+        for step_index in range(goal_steps):
+            action = random.randrange(0, 2)
+            observation, reward, done, info = env.step(action)
+            
+            if len(previous_observation) > 0:
+                game_memory.append([previous_observation, action])
+                
+            previous_observation = observation
+            score += reward
+            if done:
+                break
+            
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0, 1]
+                elif data[1] == 0:
+                    output = [1, 0]
+                training_data.append([data[0], output])
+        
+        env.reset()
+
+    print(accepted_scores)
+    
+    return training_data
+
+def build_model(input_size, output_size):
+    model = Sequential()
+    model.add(Dense(128, input_dim=input_size, activation='relu'))
+    model.add(Dense(52, activation='relu'))
+    model.add(Dense(output_size, activation='linear'))
+    model.compile(loss='mse', optimizer=Adam())
+    return model
+
+def train_model(training_data):
+    X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]))
+    y = np.array([i[1] for i in training_data]).reshape(-1, len(training_data[0][1]))
+    model = build_model(input_size=len(X[0]), output_size=len(y[0]))
+    
+    model.fit(X, y, epochs=10)
+    return model
+
+training_data = model_data_preparation()
+trained_model = train_model(training_data)
 
 
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    out = e_x / e_x.sum()
-    return out
-
-
-def policy_gradient():
-    with tf.variable_scope("policy"):
-        params = tf.get_variable("policy_parameters",[4,2])
-        state = tf.placeholder("float",[None,4])
-        actions = tf.placeholder("float",[None,2])
-        advantages = tf.placeholder("float",[None,1])
-        linear = tf.matmul(state,params)
-        probabilities = tf.nn.softmax(linear)
-        good_probabilities = tf.reduce_sum(tf.multiply(probabilities, actions),reduction_indices=[1])
-        eligibility = tf.log(good_probabilities) * advantages
-        loss = -tf.reduce_sum(eligibility)
-        optimizer = tf.train.AdamOptimizer(0.01).minimize(loss)
-        return probabilities, state, actions, advantages, optimizer
-
-def value_gradient():
-    with tf.variable_scope("value"):
-        state = tf.placeholder("float",[None,4])
-        newvals = tf.placeholder("float",[None,1])
-        w1 = tf.get_variable("w1",[4,10])
-        b1 = tf.get_variable("b1",[10])
-        h1 = tf.nn.relu(tf.matmul(state,w1) + b1)
-        w2 = tf.get_variable("w2",[10,1])
-        b2 = tf.get_variable("b2",[1])
-        calculated = tf.matmul(h1,w2) + b2
-        diffs = calculated - newvals
-        loss = tf.nn.l2_loss(diffs)
-        optimizer = tf.train.AdamOptimizer(0.1).minimize(loss)
-        return calculated, state, newvals, optimizer, loss
-
-def run_episode(env, policy_grad, value_grad, sess):
-    pl_calculated, pl_state, pl_actions, pl_advantages, pl_optimizer = policy_grad
-    vl_calculated, vl_state, vl_newvals, vl_optimizer, vl_loss = value_grad
-    observation = env.reset()
-    totalreward = 0
-    states = []
-    actions = []
-    advantages = []
-    transitions = []
-    update_vals = []
-
-
-    for _ in range(200):
-        # calculate policy
-        obs_vector = np.expand_dims(observation, axis=0)
-        probs = sess.run(pl_calculated,feed_dict={pl_state: obs_vector})
-        action = 0 if random.uniform(0,1) < probs[0][0] else 1
-        # record the transition
-        states.append(observation)
-        actionblank = np.zeros(2)
-        actionblank[action] = 1
-        actions.append(actionblank)
-        # take the action in the environment
-        old_observation = observation
-        observation, reward, done, info = env.step(action)
-        transitions.append((old_observation, action, reward))
-        totalreward += reward
-
+scores = []
+choices = []
+for each_game in range(100):
+    score = 0
+    prev_obs = []
+    for step_index in range(goal_steps):
+        # Uncomment below line if you want to see how our bot is playing the game.
+        env.render()
+        if len(prev_obs)==0:
+            action = random.randrange(0,2)
+        else:
+            action = np.argmax(trained_model.predict(prev_obs.reshape(-1, len(prev_obs)))[0])
+        
+        choices.append(action)
+        new_observation, reward, done, info = env.step(action)
+        prev_obs = new_observation
+        score+=reward
         if done:
             break
-    for index, trans in enumerate(transitions):
-        obs, action, reward = trans
 
-        # calculate discounted monte-carlo return
-        future_reward = 0
-        future_transitions = len(transitions) - index
-        decrease = 1
-        for index2 in range(future_transitions):
-            future_reward += transitions[(index2) + index][2] * decrease
-            decrease = decrease * 0.97
-        obs_vector = np.expand_dims(obs, axis=0)
-        currentval = sess.run(vl_calculated,feed_dict={vl_state: obs_vector})[0][0]
+    env.reset()
+    scores.append(score)
 
-        # advantage: how much better was this action than normal
-        advantages.append(future_reward - currentval)
-
-        # update the value function towards new return
-        update_vals.append(future_reward)
-
-    # update value function
-    update_vals_vector = np.expand_dims(update_vals, axis=1)
-    sess.run(vl_optimizer, feed_dict={vl_state: states, vl_newvals: update_vals_vector})
-    # real_vl_loss = sess.run(vl_loss, feed_dict={vl_state: states, vl_newvals: update_vals_vector})
-
-    advantages_vector = np.expand_dims(advantages, axis=1)
-    sess.run(pl_optimizer, feed_dict={pl_state: states, pl_advantages: advantages_vector, pl_actions: actions})
-
-    return totalreward
+print(scores)
+print('Average Score:',sum(scores)/len(scores))
+print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
 
 
-env = gym.make('CartPole-v0')
-# env = wrappers.Monitor(env, 'cartpole-hill/')
-# env.monitor.start('cartpole-hill/', force=True)
-policy_grad = policy_gradient()
-value_grad = value_gradient()
-sess = tf.InteractiveSession()
-sess.run(tf.initialize_all_variables())
-for i in range(2000):
-    reward = run_episode(env, policy_grad, value_grad, sess)
-    if reward == 200:
-        print("reward 200")
-        print(i)
-        break
-t = 0
-for _ in range(1000):
-    reward = run_episode(env, policy_grad, value_grad, sess)
-    t += reward
-print(t / 1000)
-# env.monitor.close()
+# for step_index in range(10000):
+#     env.render()
+#     action = env.action_space.sample()
+#     observation, reward, done, info = env.step(action)
+#     print("Step {}:".format(step_index))
+#     print("action: {}".format(action))
+#     print("observation: {}".format(observation))
+#     print("reward: {}".format(reward))
+#     print("done: {}".format(done))
+#     print("info: {}".format(info))
+#     if done:
+#         env.reset()
+
+env.close()
